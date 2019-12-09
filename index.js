78f7f2cfa87bcc2abf8ecce33402ffeac43a68c5
David <david_geran@live.de>
2019-12-09 17:31:39 +0100

    wip

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(
-	cors({ origin: 'http://whereismyrave.herokuapp.com', credentials: true })
+	cors({ origin: 'https://whereismyrave.herokuapp.com', credentials: true })
 )
 
 //GET
